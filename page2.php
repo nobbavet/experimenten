4409f93d6abb762a21510fa5983201746d953dfe
Jan Zuur <J.Zuur@fcroc.nl>
2021-01-18 09:30:38 +0100

    Mijn wijzigingen

--- page2.php
+++ page2.php
@@ -1,15 +1,6 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <nav>
-        <ul>
-            <li><a href="Elloooo.php">Menu</a></li>
-            <li><a href="page.php">Hobby's</a></li>
-        <ul>
-    </nav>
-</body>
-</html>
+
+bcscale<br>
+
+<?php
+echo "Hello world<BR>";
+?>
