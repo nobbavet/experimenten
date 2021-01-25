24fa0c5cda194fcd62e27849811212b209690688
DESKTOP-NC7PPFI\nabil ahammar <ngames1015@gmail.com>
2021-01-25 10:53:52 +0100

    no message

--- Elloooo.php
+++ Elloooo.php
@@ -1,3 +1,4 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link rel="stylesheet" href="kleurtjes.css">
 <body>
     <?php 
@@ -5,12 +6,14 @@
     ?>
 
     <?php 
+    include 'navbar.php'
+    ?>
+
+    <?php 
         echo '<p>Hello World</p>'; 
 
-        echo strrev("Nabil Ahammar");
+        echo strrev("Nabil Ahammar"); 
         echo strrev("Ik ben nabil en ben 17 jaar oud. ijn hobby zijn gamen, tekenen en fietsen/sporten.");
-        
-            include 'arrey.php';
     ?>
  
     <?php 
