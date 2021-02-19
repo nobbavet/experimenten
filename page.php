06a8bc7c1fde6a58300862ba4bd67318393db336
DESKTOP-NC7PPFI\nabil ahammar <ngames1015@gmail.com>
2021-02-19 10:12:49 +0100

    no message

--- page.php
+++ page.php
@@ -1,3 +1,7 @@
+<?php 
+require 'db_connect.php';
+?>
+
 <!-- <html>
 <head>
     <title>Page</title>
@@ -6,18 +10,35 @@
 <?php 
     //include 'navbar.php'
     ?>
-
+ 
 <body> -->
-<form action="" method="get">
-    <select name="voertuigen" id="">
-        <option>select iets</option>
-        <option value = "tank">tank</option>
-        <option value = "vliegtuig">vliegtuig</option>
-        <option value = "porche">porche</option>
-    </select>
-        <button type = "submit">submit</button>
-    </form>
-
-
-<!-- </body>
+    <div class="main-section">
+        <div class="add-section">
+            <form action="">
+                <input type="text" 
+                       name="title" 
+                       placeholder="Dit moet je invullen">
+                <button type="submit">Add &nbsp; <span>&#43;</span></button>
+            </form>
+        </div>
+        <?php
+            $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+        ?>
+        <div class="show-todo-section">
+            <?php if($todos->rowCount() === 0){ ?>
+        <div class="todo-items">
+                <div class="empty">
+                    <img src="img/load.gif" width="100%">
+                </div>
+            </div>
+            <?php } ?>
+            <div class="todo-items">
+                <input type="checkbox">
+                <h2>Eet je groentjes</h2>
+                <br>
+                <small>created: 19/02/2021</small>
+            </div>
+        </div>
+    </div>
+<!-- </body>s
 </html> -->
