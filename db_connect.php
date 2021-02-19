4b92797ba0751e5e66f618d33d930cc34dac8b3f
DESKTOP-NC7PPFI\nabil ahammar <ngames1015@gmail.com>
2021-02-19 10:13:21 +0100

    no message

--- db_connect.php
+++ db_connect.php
@@ -6,7 +6,11 @@ $Pass = "";
 $db_name = "to_do_list";
 
 try {
+<<<<<<< HEAD
     $conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $Pass);
+=======
+    $conn = new PDO("mysql:host=$sName;db_name=$db_name", $uName, $Pass);
+>>>>>>> 7aafe0e6626e26a183ca57aec740f8468af96160
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "connected :D";
 }catch(PDOException $e){
--- page.php
+++ page.php
@@ -12,6 +12,10 @@ require 'db_connect.php';
     ?>
  
 <body> -->
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 7aafe0e6626e26a183ca57aec740f8468af96160
     <div class="main-section">
         <div class="add-section">
             <form action="">
@@ -22,6 +26,7 @@ require 'db_connect.php';
             </form>
         </div>
         <?php
+<<<<<<< HEAD
             $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
         ?>
         <div class="show-todo-section">
@@ -32,6 +37,11 @@ require 'db_connect.php';
                 </div>
             </div>
             <?php } ?>
+=======
+
+        ?>
+        <div class="show-todo-section">
+>>>>>>> 7aafe0e6626e26a183ca57aec740f8468af96160
             <div class="todo-items">
                 <input type="checkbox">
                 <h2>Eet je groentjes</h2>
@@ -41,4 +51,23 @@ require 'db_connect.php';
         </div>
     </div>
 <!-- </body>s
+<<<<<<< HEAD
+=======
+=======
+<form action="" method="get">
+    <select name="voertuigen" id="">
+        <option>select iets</option>
+        <option value = "tank">tank</option>
+        <option value = "vliegtuig">vliegtuig</option>
+        <option value = "porche">porche</option>
+    </select>
+        <button type = "submit">submit</button>
+    </form>
+
+<?php 
+
+?>
+<!-- </body>
+>>>>>>> 7a18fbf348ffd3a53a8e3260bb2fa13c64f2634d
+>>>>>>> 7aafe0e6626e26a183ca57aec740f8468af96160
 </html> -->
