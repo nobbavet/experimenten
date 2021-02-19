f63b4ffca4af9ba86652f44c8a91cb7d9282767a
DESKTOP-NC7PPFI\nabil ahammar <ngames1015@gmail.com>
2021-02-19 12:51:29 +0100

    1 grotee mislukking toevoegen lukt maar verwijderen of de vinkjes behouden werkt niet :(

--- application/addon.php
+++ application/addon.php
@@ -1,11 +1,16 @@
 <?php 
 
 if(isset($_POST['title'])){
+<<<<<<< HEAD
     require '../db_connect.php';
+=======
+    //require '';
+>>>>>>> e36e2fe49cfe016e369e54b864c8172f468e361c
 
     $title = $_POST['title'];
 
     if(empty($title)){
+<<<<<<< HEAD
         header("Location: ../page.php?mess=error");
     }else {
         $stmt = $conn->prepare("INSERT INTO todos(title) VALUE(?)");
@@ -21,4 +26,8 @@ if(isset($_POST['title'])){
     }
 }else {
     header("Location: ../page.php?mess=error");
+=======
+        header("Location: ../index.php?mess=error");
+    }
+>>>>>>> e36e2fe49cfe016e369e54b864c8172f468e361c
 }
