95045996c027c9747b72163e170348f8be509870
Jan Zuur <J.Zuur@fcroc.nl>
2021-02-25 15:59:06 +0100

    Revert "Deze nog vergeten mee te leveren ;-)"
    This reverts commit 23bc13cc48cca37f1920a21ac39c55598bdd08e4.

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
-<?php 
-    require 'db_connect.php';
-?>
-<html>
-    <head>
-    <link rel="stylesheet" href="kleurtjes.css">
-    </head>
-    <body>
-        <?php
-            $page = (isset($_GET['i']))?$_GET['i']:'home';
-            // zuja: probeer dit meer functie gestuurd te maken. Hier staat nog te veel data die gedumpt wordt.
-            // hierdoor krijg je veel HTML met PHP stukjes. Draai dit eens om.
-            include 'header.php';
-            include 'navbar.php';
+<link rel="stylesheet" href="kleurtjes.css">
 
-            switch ($page) {
-                case 'home': include 'home.php'; break;
-                case 'p1': include 'page.php'; break;
-                case 'p2': include 'page2.php'; break;
-            }
-            echo content();
-            include 'footer.php'; 
-        //   include 'page.php';
-        //   include 'page2.php';
-        ?>
-    </body>
-</html>
+
+<body>
+    <?php
+        $page = (isset($_GET['i']))?$_GET['i']:'home';
+        include 'header.php';
+        include 'navbar.php';
+
+        switch ($page) {
+            case 'home':
+                echo '<p>Hello World</p>'; 
+                echo strrev("Nabil Ahammar"); 
+                echo strrev("Ik ben nabil en ben 17 jaar oud. ijn hobby zijn gamen, tekenen en fietsen/sporten.");
+                break;
+            case 'p1': include 'page.php'; break;
+            case 'p2': include 'page2.php'; break;
+        }
+    ?>
+ 
+
+ 
+    <?php 
+    include 'footer.php'; 
+    ?>
+    
+    
+    <?php 
+//   include 'page.php';
+//   include 'page2.php';
+    ?>
+</body>
